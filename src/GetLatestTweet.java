0850654e98455009d0f1da51bf48e2126e7713be
Itscaliform <itscaliform@gmail.com>
2018-03-05 16:57:47 -0800

    Add files via upload
    this is my program that works

---
+++ src/GetLatestTweet.java
@@ -0,0 +1,72 @@
+import java.awt.Frame;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+import twitter4j.Query;
+import twitter4j.QueryResult;
+import twitter4j.Twitter;
+import twitter4j.TwitterFactory;
+import twitter4j.auth.AccessToken;
+
+
+public class GetLatestTweet implements ActionListener {
+	 JPanel Panel;
+	JFrame frame;
+	JButton Button;
+	JTextField textField;
+	
+	public void createUI() {
+		
+		frame = new JFrame();
+		Panel = new JPanel();
+		textField = new JTextField(25);
+		Button = new JButton("get latest tweet");
+		
+		frame.add(Panel);
+		Panel.add(textField);
+		Panel.add(Button);
+		Button.addActionListener(this);
+		frame.pack();
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+	
+	
+	public static void main(String[] args) {
+		new GetLatestTweet().createUI();
+	
+}
+
+    private String getLatestTweet(String searchingFor) {
+
+        Twitter twitter = new TwitterFactory().getInstance();
+        AccessToken accessToken = new AccessToken(
+                "2453751158-IVD2VGZsvwZiRKxNe3Gs2lMjg30nvSkV1xSuPFf",
+                "vBa5PjKfuMTK1LLBG51nCUI9r5d6Ph5cAHrS73spg6Nvu");
+        twitter.setOAuthConsumer("YqeZdD2hYxOKv4QOkmp0i2djN",
+                "6XUB1r8KXBvd8Pk9HHW3NgphMxHvHWBLAr5TihnckMU0ttyGST");
+        twitter.setOAuthAccessToken(accessToken);
+
+        Query query = new Query(searchingFor);
+        try {
+            QueryResult result = twitter.search(query);
+            return result.getTweets().get(0).getText();
+        } catch (Exception e) {
+            System.err.print(e.getMessage());
+            return "What the heck is that?";
+        }
+    }
+
+
+
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}}
