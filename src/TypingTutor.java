f0f70fb0f65c1e30ca5121aa8df15fa143929f51
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-03-19 16:48:49 -0700

    tids iufgnj

--- src/TypingTutor.java
+++ src/TypingTutor.java
@@ -4,6 +4,8 @@
 import java.awt.Frame;
 	import java.awt.event.ActionEvent;
 	import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.util.Random;
 
 import javax.swing.JButton;
@@ -15,22 +17,24 @@ import javax.swing.JPanel;
 	
 
 
-	public class TypingTutor implements ActionListener {
+	public class TypingTutor implements ActionListener, KeyListener {
 		 char currentLetter = generateRandomLetter();
 		 JLabel label;
 		 JPanel Panel;
 		JFrame frame;
 	String robert= currentLetter+ "";
 		public void createUI() {
-		
+			  
 		label= new JLabel();
 			frame = new JFrame();
 			Panel = new JPanel();
 			frame.setPreferredSize(	new Dimension(800,600));
 			label.setText(robert);
+			label.setFont(label.getFont().deriveFont(28.0f));
+			label.setHorizontalAlignment(JLabel.CENTER);  
 			frame.add(Panel);
 			Panel.add(label);
-			
+			frame.addKeyListener(this);
 			
 			frame.pack();
 			frame.setVisible(true);
@@ -52,6 +56,38 @@ import javax.swing.JPanel;
 
 		@Override
 		public void actionPerformed(ActionEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+
+		@Override
+		public void keyPressed(KeyEvent arg0) {
+			// TODO Auto-generated method stub
+			System.out.println("you typed: "+arg0.getKeyChar());
+			if(arg0.getKeyChar()==currentLetter) {
+				System.out.println("correct");
+				
+			}
+			else {
+				System.out.println("wrong");
+			}
+		}
+
+
+		@Override
+		public void keyReleased(KeyEvent e) {
+			// TODO Auto-generated method stub
+			
+			currentLetter = generateRandomLetter();
+			String roberto= currentLetter+ "";
+		label.setText(roberto);
+		
+		}
+
+
+		@Override
+		public void keyTyped(KeyEvent e) {
 			// TODO Auto-generated method stub
 			
 		}
