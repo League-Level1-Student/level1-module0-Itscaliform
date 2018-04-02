60035d1d3684c3259f051f4a516d940c63f9632c
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-04-02 16:51:30 -0700

    i completed literally a lot of thingd

---
+++ src/RockStar.java
@@ -0,0 +1,31 @@
+
+public class RockStar {
+public static void main(String[] args) {
+//weRock();
+	//rocker("roberto");
+	//truth("Post Malone");
+	echo("hello");
+}
+static void weRock() {
+	System.out.println("The League rocks");
+	
+	
+}
+static void rocker(String rocker) {
+	System.out.println(rocker+" rocks");
+}
+static void truth(String singer) {
+	if(singer.equals("Post Malone")) {
+		System.out.println(singer+" is awesome");
+	}
+	else {
+		System.out.println(singer+ "is annoying");
+	}
+	
+}
+static void echo(String echo) {
+	System.out.println(echo+" "+echo);
+	
+	
+}
+}
--- src/TypingTutor.java
+++ src/TypingTutor.java
@@ -1,16 +1,19 @@
 
 
-	import java.awt.Dimension;
+	import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Frame;
 	import java.awt.event.ActionEvent;
 	import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.Date;
 import java.util.Random;
 
 import javax.swing.JButton;
 	import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 	import javax.swing.JTextField;
 
@@ -18,6 +21,7 @@ import javax.swing.JPanel;
 
 
 	public class TypingTutor implements ActionListener, KeyListener {
+
 		 char currentLetter = generateRandomLetter();
 		 JLabel label;
 		 JPanel Panel;
@@ -59,18 +63,23 @@ import javax.swing.JPanel;
 			// TODO Auto-generated method stub
 			
 		}
-
+int counter;
 
 		@Override
 		public void keyPressed(KeyEvent arg0) {
 			// TODO Auto-generated method stub
+			counter++;
 			System.out.println("you typed: "+arg0.getKeyChar());
 			if(arg0.getKeyChar()==currentLetter) {
 				System.out.println("correct");
-	
+			 Panel.setBackground(Color.green);
+			 if (counter>5) {
+			 showTypingSpeed(counter);
+			}
 			}
 			else {
 				System.out.println("wrong");
+				Panel.setBackground(Color.red);
 			}
 		}
 
@@ -91,4 +100,14 @@ import javax.swing.JPanel;
 			// TODO Auto-generated method stub
 			
 		}
+		private void showTypingSpeed(int numberOfCorrectCharactersTyped) {
+		    Date timeAtEnd = new Date();
+		    long gameDuration = timeAtEnd.getTime() - timeAtStart.getTime();
+		    long gameInSeconds = (gameDuration / 1000) % 60;
+		    double charactersPerSecond = ((double) numberOfCorrectCharactersTyped / (double) gameInSeconds);
+		    int charactersPerMinute = (int) (charactersPerSecond * 60);
+		System.out.println("Your typing speed is " + charactersPerMinute + " characters per minute.");
+		}
+		Date timeAtStart = new Date();
+
 }
