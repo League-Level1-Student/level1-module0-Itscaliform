cc0c783a3a773ad681e1b5af0606111e8dccb971
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-04-09 16:51:26 -0700

    okay

--- src/RockStar.java
+++ src/RockStar.java
@@ -5,7 +5,35 @@ public static void main(String[] args) {
 	//rocker("roberto");
 	//truth("Post Malone");
 	//echo("hello");
+	//repeat("mayday", 4);
+	//String theBest = getTheBestCoder();
+	//System.out.println(theBest); 
+	 //int sum = add(2,2);
+	 //System.out.println(sum); //prints 4 
+	//oddorna(6);
+}
+static int oddorna(int r){
+	if (r/3==1) {
+		
+		System.out.println("odd");
+	}
+	else {
+		System.out.println("naaaa");
+	}
 	
+	return r;	
+}
+private static int add(int i, int j) {
+	// TODO Auto-generated method stub
+	int num = i+j;
+	
+	return num;
+}
+private static String getTheBestCoder() {
+	// TODO Auto-generated method stub
+	String name = "roberto williams";
+	
+	return name;
 }
 static void weRock() {
 	System.out.println("The League rocks");
@@ -29,5 +57,12 @@ static void echo(String echo) {
 	
 	
 }
+static void repeat(String name, int num) {
+	for (int i = 0; i <num; i++) {
+		
+	
+	System.out.println(name);
+}
 
+}
 }
