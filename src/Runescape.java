d75bcecf96f4f80762e59b669b82e4f307eb0281
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-04-16 16:50:23 -0700

    We made stuff

---
+++ src/Runescape.java
@@ -0,0 +1,16 @@
+import javax.swing.JOptionPane;
+
+public class Runescape {
+public static void main(String[] args) {
+	
+}
+String them;
+String Attack;
+public void theme(String theme) {
+this.them=theme;
+}
+
+public void setAttack(String Attack) {
+this.Attack=Attack;
+}
+}
---
+++ src/rune.java
@@ -0,0 +1,11 @@
+
+public class rune {
+
+public static void main(String[] args) {
+	Runescape robert= new Runescape();
+	robert.them="red";
+	robert.Attack="kick";
+	System.out.println(robert.Attack);
+	System.out.println(robert.them);
+}
+}
